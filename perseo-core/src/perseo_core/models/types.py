f94a0feca9e407f512b6ab2ea8b9425a2776ffea
Giorgio Parma <giorgio.parma@aresys.it>
2026-02-13 11:43:16 +0100

    core: adding new types

--- perseo-core/src/perseo_core/models/types.py
+++ perseo-core/src/perseo_core/models/types.py
@@ -15,5 +15,11 @@ from perseo_core.timing.precise_datetime import PreciseDateTime
 
 ExtendedDatetimeType = PreciseDateTime | np.datetime64
 ExtendedDatetimeArrayType = NDArray[Shape["*"], ExtendedDatetimeType]  # type: ignore
+PreciseDateTimeArrayType = NDArray[Shape["*"], PreciseDateTime]  # type: ignore
+NumpyDateTimeArrayType = NDArray[Shape["*"], np.datetime64]  # type: ignore
 CoordinatesArrayType = NDArray[Shape["* x, * y, * z"], float] | NDArray[Shape["3"], float]  # type: ignore
 FloatArrayType = NDArray[Shape["*"], float]  # type: ignore
+ROIArrayType = NDArray[Shape["* samples, * lines"], float] | NDArray[Shape["2"], float]  # type: ignore
+InverseROIArrayType = NDArray[Shape["* lines, * samples"], float] | NDArray[Shape["2"], float]  # type: ignore
+ReferenceFrameType = NDArray[Shape["3, 3"], float]  # type: ignore
+ReferenceFrameArrayType = NDArray[Shape["*, 3, 3"], float]  # type: ignore
