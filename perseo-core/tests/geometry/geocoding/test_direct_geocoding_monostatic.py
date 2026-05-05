0b3f38dddf6f5a146135b85c4f41812718329c28
Giorgio Parma <giorgio.parma@aresys.it>
2026-05-05 10:41:40 +0200

    core: adding tests for range vectorized direct_geocoding_monostatic function

--- perseo-core/tests/geometry/geocoding/test_direct_geocoding_monostatic.py
+++ perseo-core/tests/geometry/geocoding/test_direct_geocoding_monostatic.py
@@ -16,6 +16,7 @@ from perseo_core.geometry.geocoding.direct_geocoding_core import (
     _doppler_equation,
     _ellipse_equation,
     direct_geocoding_monostatic_core,
+    direct_geocoding_monostatic_core_range_vectorized,
 )
 from perseo_core.geometry.utilities.ellipsoid import WGS84
 from perseo_core.models.enums import SensorLookDirection
@@ -130,6 +131,7 @@ class DirectGeocodingMonostaticTest(unittest.TestCase):
         self.wavelength = 1
         self.N = 4
         self.M = 5
+        self.Q = 120
 
         self.tolerance = 1e-6
         self.residual_tolerance = 1e-8
@@ -284,6 +286,72 @@ class DirectGeocodingMonostaticTest(unittest.TestCase):
                 "check_residuals": True,
             },
             {
+                "name": "case2f: 1 pos (3,), 1 vel (3,), Q rng times (M,), 1 initial guess (3,)",
+                "positions": self.positions,
+                "velocities": self.scaled_arf_velocities,
+                "initial_guesses": self.initial_guesses,
+                "range_times": np.repeat(self.range_times[0], self.Q),
+                "doppler_frequencies": self.doppler_freqs,
+                "expected_ndim": 2,
+                "expected_shape": (self.Q, 3),
+                "check_residuals": True,
+            },
+            {
+                "name": "case2g: 1 pos (1,3), 1 vel (1,3), Q rng times (Q,), 1 initial guess (1,3)",
+                "positions": self.positions.reshape(1, 3),
+                "velocities": self.scaled_arf_velocities.reshape(1, 3),
+                "initial_guesses": self.initial_guesses.reshape(1, 3),
+                "range_times": np.repeat(self.range_times[0], self.Q),
+                "doppler_frequencies": self.doppler_freqs,
+                "expected_ndim": 2,
+                "expected_shape": (self.Q, 3),
+                "check_residuals": True,
+            },
+            {
+                "name": "case2h: 1 pos (3,), 1 vel (3,), Q rng times (Q,), no initial guess",
+                "positions": self.positions,
+                "velocities": self.scaled_arf_velocities,
+                "initial_guesses": None,
+                "range_times": np.repeat(self.range_times[0], self.Q),
+                "doppler_frequencies": self.doppler_freqs,
+                "expected_ndim": 2,
+                "expected_shape": (self.Q, 3),
+                "check_residuals": True,
+            },
+            {
+                "name": "case2i: 1 pos (3,), 1 vel (3,), Q rng times (Q,), 1 initial guess (3,), Q doppler freqs",
+                "positions": self.positions,
+                "velocities": self.scaled_arf_velocities,
+                "initial_guesses": self.initial_guesses,
+                "range_times": np.repeat(self.range_times[0], self.Q),
+                "doppler_frequencies": np.repeat(self.doppler_freqs, self.Q),
+                "expected_ndim": 2,
+                "expected_shape": (self.Q, 3),
+                "check_residuals": True,
+            },
+            {
+                "name": "case2j: 1 pos (1,3), 1 vel (1,3), Q rng times (Q,), 1 initial guess (1,3), Q doppler freqs",
+                "positions": self.positions.reshape(1, 3),
+                "velocities": self.scaled_arf_velocities.reshape(1, 3),
+                "initial_guesses": self.initial_guesses.reshape(1, 3),
+                "range_times": np.repeat(self.range_times[0], self.Q),
+                "doppler_frequencies": np.repeat(self.doppler_freqs, self.Q),
+                "expected_ndim": 2,
+                "expected_shape": (self.Q, 3),
+                "check_residuals": True,
+            },
+            {
+                "name": "case2k: 1 pos (1,3), 1 vel (1,3), Q rng times (Q,), Q initial guesses (Q,3), Q doppler freqs",
+                "positions": self.positions.reshape(1, 3),
+                "velocities": self.scaled_arf_velocities.reshape(1, 3),
+                "initial_guesses": np.full((self.Q, 3), self.initial_guesses),
+                "range_times": np.repeat(self.range_times[0], self.Q),
+                "doppler_frequencies": np.repeat(self.doppler_freqs, self.Q),
+                "expected_ndim": 2,
+                "expected_shape": (self.Q, 3),
+                "check_residuals": True,
+            },
+            {
                 "name": "case3a: N pos (N,3), N vel (N,3), M rng times (M,), 1 initial guess (3,)",
                 "positions": np.full((self.N, 3), self.positions),
                 "velocities": np.full((self.N, 3), self.scaled_arf_velocities),
@@ -314,6 +382,50 @@ class DirectGeocodingMonostaticTest(unittest.TestCase):
                 "doppler_frequencies": np.repeat(self.doppler_freqs, self.M),
                 "expected_ndim": 3,
                 "expected_shape": (self.N, self.M, 3),
+                "check_residuals": True,
+            },
+            {
+                "name": "case3d: N pos (N,3), N vel (N,3), Q rng times (Q,), 1 initial guess (3,)",
+                "positions": np.full((self.N, 3), self.positions),
+                "velocities": np.full((self.N, 3), self.scaled_arf_velocities),
+                "initial_guesses": self.initial_guesses,
+                "range_times": np.repeat(self.range_times[0], self.Q),
+                "doppler_frequencies": self.doppler_freqs,
+                "expected_ndim": 3,
+                "expected_shape": (self.N, self.Q, 3),
+                "check_residuals": True,
+            },
+            {
+                "name": "case3e: N pos (N,3), N vel (N,3), Q rng times (Q,), no initial guess",
+                "positions": np.full((self.N, 3), self.positions),
+                "velocities": np.full((self.N, 3), self.scaled_arf_velocities),
+                "initial_guesses": None,
+                "range_times": np.repeat(self.range_times[0], self.Q),
+                "doppler_frequencies": self.doppler_freqs,
+                "expected_ndim": 3,
+                "expected_shape": (self.N, self.Q, 3),
+                "check_residuals": True,
+            },
+            {
+                "name": "case3f: N pos (N,3), N vel (N,3), Q rng times (Q,), 1 initial guess (3,), Q doppler freqs",
+                "positions": np.full((self.N, 3), self.positions),
+                "velocities": np.full((self.N, 3), self.scaled_arf_velocities),
+                "initial_guesses": self.initial_guesses,
+                "range_times": np.repeat(self.range_times[0], self.Q),
+                "doppler_frequencies": np.repeat(self.doppler_freqs, self.Q),
+                "expected_ndim": 3,
+                "expected_shape": (self.N, self.Q, 3),
+                "check_residuals": True,
+            },
+            {
+                "name": "case3g: N pos (N,3), N vel (N,3), Q rng times (Q,), Q initial guesses (Q,3)",
+                "positions": np.full((self.N, 3), self.positions),
+                "velocities": np.full((self.N, 3), self.scaled_arf_velocities),
+                "initial_guesses": np.full((self.Q, 3), self.initial_guesses),
+                "range_times": np.repeat(self.range_times[0], self.Q),
+                "doppler_frequencies": np.repeat(self.doppler_freqs, self.Q),
+                "expected_ndim": 3,
+                "expected_shape": (self.N, self.Q, 3),
                 "check_residuals": True,
             },
         ]
@@ -577,6 +689,224 @@ class DirectGeocodingMonostaticCoreTest(unittest.TestCase):
             )
 
 
+class DirectGeocodingRangeVectorizedMonostaticCoreTest(unittest.TestCase):
+    """Testing direct geocoding monostatic range vectorized core with various input combinations using subtests."""
+
+    def setUp(self):
+        """Setting up variables for testing"""
+        self.position = np.array(
+            [4387348.749948771, 762123.3489877012, 4553067.931912004],
+        )
+        self.velocity = np.array(
+            [-856.1384108174528, -329.7629775067583, 398.55830806407346],
+        )
+        self.initial_guess = np.array([4385882.165360568, 764600.914414172, 4551967.490551733])
+        self.range_time = np.array([2.05624579e-05])
+        self.doppler_freq = 0
+        self.geodetic_altitude = 0
+        self.wavelength = 1
+
+        self.N = 4
+        self.M = 100
+        self.tolerance = 1e-6
+
+        self.results = np.array([4385882.195057692, 764600.9869913795, 4551967.6143934])
+
+    def test_monostatic_core_cases(self) -> None:
+        """Test direct_geocoding_monostatic_core_range_vectorized with various input combinations using subtests."""
+
+        test_cases = [
+            {
+                "name": "case0a: 1 pos (3,), 1 vel (3,), 1 guess (3,), 1 rng time",
+                "positions": self.position,
+                "velocities": self.velocity,
+                "initial_guesses": self.initial_guess,
+                "range_times": self.range_time[0],
+                "doppler_frequencies": self.doppler_freq,
+                "expected_ndim": 1,
+                "expected_shape": (3,),
+            },
+            {
+                "name": "case0b: 1 pos (1,3), 1 vel (1,3), 1 guess (1,3), 1 rng time",
+                "positions": self.position.reshape(1, 3),
+                "velocities": self.velocity.reshape(1, 3),
+                "initial_guesses": self.initial_guess.reshape(1, 3),
+                "range_times": self.range_time[0],
+                "doppler_frequencies": self.doppler_freq,
+                "expected_ndim": 2,
+                "expected_shape": (1, 3),
+            },
+            {
+                "name": "case0c: 1 pos (3,), 1 vel (3,), 1 guess (3,), M range times",
+                "positions": self.position,
+                "velocities": self.velocity,
+                "initial_guesses": self.initial_guess,
+                "range_times": np.repeat(self.range_time[0], self.M),
+                "doppler_frequencies": self.doppler_freq,
+                "expected_ndim": 2,
+                "expected_shape": (self.M, 3),
+            },
+            {
+                "name": "case0d: 1 pos (1,3), 1 vel (1,3), 1 guess (1,3), M range times",
+                "positions": self.position.reshape(1, 3),
+                "velocities": self.velocity.reshape(1, 3),
+                "initial_guesses": self.initial_guess.reshape(1, 3),
+                "range_times": np.repeat(self.range_time[0], self.M),
+                "doppler_frequencies": self.doppler_freq,
+                "expected_ndim": 2,
+                "expected_shape": (self.M, 3),
+            },
+            {
+                "name": "case0e: 1 pos (1,3), 1 vel (1,3), M guess (M,3), M range times",
+                "positions": self.position.reshape(1, 3),
+                "velocities": self.velocity.reshape(1, 3),
+                "initial_guesses": np.full((self.M, 3), self.initial_guess),
+                "range_times": np.repeat(self.range_time[0], self.M),
+                "doppler_frequencies": self.doppler_freq,
+                "expected_ndim": 2,
+                "expected_shape": (self.M, 3),
+            },
+            {
+                "name": "case0f: 1 pos (3,), 1 vel (3,), 1 guess (3,), M range times, M doppler freqs",
+                "positions": self.position,
+                "velocities": self.velocity,
+                "initial_guesses": self.initial_guess,
+                "range_times": np.repeat(self.range_time[0], self.M),
+                "doppler_frequencies": np.repeat(self.doppler_freq, self.M),
+                "expected_ndim": 2,
+                "expected_shape": (self.M, 3),
+            },
+            {
+                "name": "case0g: 1 pos (1,3), 1 vel (1,3), 1 guess (1,3), M range times, M doppler freqs",
+                "positions": self.position.reshape(1, 3),
+                "velocities": self.velocity.reshape(1, 3),
+                "initial_guesses": self.initial_guess.reshape(1, 3),
+                "range_times": np.repeat(self.range_time[0], self.M),
+                "doppler_frequencies": np.repeat(self.doppler_freq, self.M),
+                "expected_ndim": 2,
+                "expected_shape": (self.M, 3),
+            },
+            {
+                "name": "case0h: 1 pos (1,3), 1 vel (1,3), M guess (M,3), M range times, M doppler freqs",
+                "positions": self.position.reshape(1, 3),
+                "velocities": self.velocity.reshape(1, 3),
+                "initial_guesses": np.full((self.M, 3), self.initial_guess),
+                "range_times": np.repeat(self.range_time[0], self.M),
+                "doppler_frequencies": np.repeat(self.doppler_freq, self.M),
+                "expected_ndim": 2,
+                "expected_shape": (self.M, 3),
+            },
+            {
+                "name": "case1a: N pos (N,3), N vel (N,3), N guess (N,3), 1 rng time",
+                "positions": np.full((self.N, 3), self.position),
+                "velocities": np.full((self.N, 3), self.velocity),
+                "initial_guesses": np.full((self.N, 3), self.initial_guess),
+                "range_times": self.range_time[0],
+                "doppler_frequencies": self.doppler_freq,
+                "expected_ndim": 2,
+                "expected_shape": (self.N, 3),
+            },
+            {
+                "name": "case1b: N pos (N,3), N vel (N,3), N guess (N,3), M rng times",
+                "positions": np.full((self.N, 3), self.position),
+                "velocities": np.full((self.N, 3), self.velocity),
+                "initial_guesses": np.full((self.N, 3), self.initial_guess),
+                "range_times": np.repeat(self.range_time[0], self.M),
+                "doppler_frequencies": self.doppler_freq,
+                "expected_ndim": 3,
+                "expected_shape": (self.N, self.M, 3),
+            },
+            {
+                "name": "case1c: N pos (N,3), N vel (N,3), M guess (M,3), M rng times",
+                "positions": np.full((self.N, 3), self.position),
+                "velocities": np.full((self.N, 3), self.velocity),
+                "initial_guesses": np.full((self.M, 3), self.initial_guess),
+                "range_times": np.repeat(self.range_time[0], self.M),
+                "doppler_frequencies": self.doppler_freq,
+                "expected_ndim": 3,
+                "expected_shape": (self.N, self.M, 3),
+            },
+            {
+                "name": "case1d: N pos (N,3), N vel (N,3), N guess (N,3), M rng times, M doppler freqs",
+                "positions": np.full((self.N, 3), self.position),
+                "velocities": np.full((self.N, 3), self.velocity),
+                "initial_guesses": np.full((self.N, 3), self.initial_guess),
+                "range_times": np.repeat(self.range_time[0], self.M),
+                "doppler_frequencies": np.repeat(self.doppler_freq, self.M),
+                "expected_ndim": 3,
+                "expected_shape": (self.N, self.M, 3),
+            },
+            {
+                "name": "case1e: N pos (N,3), N vel (N,3), M guess (M,3), M rng times, M doppler freqs",
+                "positions": np.full((self.N, 3), self.position),
+                "velocities": np.full((self.N, 3), self.velocity),
+                "initial_guesses": np.full((self.M, 3), self.initial_guess),
+                "range_times": np.repeat(self.range_time[0], self.M),
+                "doppler_frequencies": np.repeat(self.doppler_freq, self.M),
+                "expected_ndim": 3,
+                "expected_shape": (self.N, self.M, 3),
+            },
+        ]
+
+        for case in test_cases:
+            with self.subTest(case=case["name"]):
+                out = direct_geocoding_monostatic_core_range_vectorized(
+                    sensor_positions=case["positions"],
+                    sensor_velocities=case["velocities"],
+                    initial_guesses=case["initial_guesses"],
+                    range_times=case["range_times"],
+                    doppler_frequencies=case["doppler_frequencies"],
+                    wavelength=self.wavelength,
+                    altitude=self.geodetic_altitude,
+                )
+
+                self.assertEqual(out.ndim, case["expected_ndim"])
+                self.assertEqual(out.shape, case["expected_shape"])
+
+                # Determine expected result shape
+                if case["expected_shape"] == (3,):
+                    expected_result = self.results
+                elif case["expected_shape"] == (1, 3):
+                    expected_result = self.results.reshape(1, 3)
+                elif case["expected_shape"] == (self.M, 3):
+                    expected_result = np.full((self.M, 3), self.results)
+                elif case["expected_shape"] == (self.N, 3):
+                    expected_result = np.full((self.N, 3), self.results)
+                elif case["expected_shape"] == (self.N, self.M, 3):
+                    expected_result = np.full((self.N, self.M, 3), self.results)
+                else:
+                    expected_result = self.results
+
+                np.testing.assert_allclose(out, expected_result, atol=self.tolerance, rtol=0)
+
+    def test_monostatic_core_error_case(self) -> None:
+        """Test direct_geocoding_monostatic_core error handling for mismatched dimensions."""
+
+        # case: M range (M,), N doppler freqs (N,), mismatched frequencies/ranges
+        with self.assertRaises(RuntimeError):
+            direct_geocoding_monostatic_core(
+                sensor_positions=np.full((self.N, 3), self.position),
+                sensor_velocities=np.full((self.N, 3), self.velocity),
+                initial_guesses=np.full((self.N, 3), self.initial_guess),
+                range_times=np.repeat(self.range_time[0], self.M),
+                doppler_frequencies=np.repeat(self.doppler_freq, self.N),
+                wavelength=self.wavelength,
+                altitude=self.geodetic_altitude,
+            )
+
+        # case: M range (M,), Q initial guesses (Q,), mismatched initial guesses/ranges
+        with self.assertRaises(RuntimeError):
+            direct_geocoding_monostatic_core(
+                sensor_positions=np.full((self.N, 3), self.position),
+                sensor_velocities=np.full((self.N, 3), self.velocity),
+                initial_guesses=np.full((17, 3), self.initial_guess),
+                range_times=np.repeat(self.range_time[0], self.M),
+                doppler_frequencies=np.repeat(self.doppler_freq, self.N),
+                wavelength=self.wavelength,
+                altitude=self.geodetic_altitude,
+            )
+
+
 class NewtonForDirectGeocodingMonostaticTest(unittest.TestCase):
     """Testing Newton method for direct geocoding monostatic using subtests."""
 
@@ -628,10 +958,10 @@ class NewtonForDirectGeocodingMonostaticTest(unittest.TestCase):
                     sensor_positions=case["positions"],
                     sensor_velocities=case["velocities"],
                     initial_guesses=case["initial_guesses"],
-                    range_time=self.range_time,
+                    range_times=self.range_time,
                     altitude=self.geodetic_altitude,
                     wavelength=self.wavelength,
-                    doppler_frequency=self.doppler_frequency,
+                    doppler_frequencies=self.doppler_frequency,
                 )
 
                 self.assertEqual(out.ndim, case["expected_ndim"])
