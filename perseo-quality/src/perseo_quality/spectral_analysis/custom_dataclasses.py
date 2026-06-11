8c3a81c7183e297e7589ff28404aab07144c0375
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-11 10:28:41 +0200

    quality: reducing duplication in spectral analysis dataclasses

--- perseo-quality/src/perseo_quality/spectral_analysis/custom_dataclasses.py
+++ perseo-quality/src/perseo_quality/spectral_analysis/custom_dataclasses.py
@@ -30,6 +30,56 @@ class SpectralAnalysisProductGeneralInfo:
 
 
 @dataclass
+class SpectralAnalysisCoreInfo:
+    """Output core results for single Target Spectral Analysis"""
+
+    azimuth_frequency_axis: npt.NDArray[np.floating] | None = None
+    range_frequency_axis: npt.NDArray[np.floating] | None = None
+    spectrum_db: npt.NDArray[np.floating] | None = None
+    spectrum_deg: npt.NDArray[np.floating] | None = None
+    spectrogram_db: npt.NDArray[np.floating] | None = None
+    spectrogram_frequencies: npt.NDArray[np.floating] | None = None
+    spectrogram_times: npt.NDArray[np.floating] | None = None
+    range_profiles_db: list[npt.NDArray[np.floating]] | None = None
+    azimuth_profiles_db: list[npt.NDArray[np.floating]] | None = None
+    range_profiles_deg: list[npt.NDArray[np.floating]] | None = None
+    azimuth_profiles_deg: list[npt.NDArray[np.floating]] | None = None
+
+
+@dataclass
+class SpectralAnalysisTargetInfo(SpectralAnalysisCoreInfo):
+    """Storing data for Point Target Spectral Analysis graphs"""
+
+    target_name: str | None = None
+    burst: int | None = None
+    roi_size_azimuth: int | None = None
+    roi_size_range: int | None = None
+    target_azimuth_pixel: float | None = None
+    target_range_pixel: float | None = None
+    azimuth_time: PreciseDateTime | None = None
+    range_time: float | None = None
+    range_profiles_norm_deg: list[npt.NDArray[np.floating]] | None = None
+    azimuth_profiles_norm_deg: list[npt.NDArray[np.floating]] | None = None
+    target_phase_value_deg: float | None = None
+    target_doppler_centroid_Hz: float | None = None
+    range_polynomial_fit: Polynomial | None = None
+    azimuth_polynomial_fit: Polynomial | None = None
+    rng_spectrum_boundaries: tuple[float, float] | None = None
+    az_spectrum_boundaries: tuple[float, float] | None = None
+
+
+@dataclass
+class SpectralAnalysisBlockInfo(SpectralAnalysisCoreInfo):
+    """Block-wise Spectral Analysis info"""
+
+    block_num: int | None = None
+    first_az_line_block: int | None = None
+    lines_block: int | None = None
+    samples_block: int | None = None
+    doppler_centroid_mid_block: float | None = None
+
+
+@dataclass
 class PointTargetSpectraDataOutput:
     """Storing data for Point Target Spectral Analysis graphs"""
 
@@ -43,58 +93,3 @@ class DistributedSpectraDataOutput:
 
     general_info: SpectralAnalysisProductGeneralInfo | None = None
     blocks_info: list[SpectralAnalysisBlockInfo] | None = None
-
-
-@dataclass
-class SpectralAnalysisTargetInfo:
-    """Storing data for Point TargetSpectral Analysis graphs"""
-
-    target_name: str | None = None
-    burst: int | None = None
-    roi_size_azimuth: int | None = None
-    roi_size_range: int | None = None
-    target_azimuth_pixel: float | None = None
-    target_range_pixel: float | None = None
-    azimuth_time: PreciseDateTime | None = None
-    range_time: float | None = None
-    azimuth_frequency_axis: npt.NDArray[np.floating] | None = None
-    range_frequency_axis: npt.NDArray[np.floating] | None = None
-    spectrum_db: npt.NDArray[np.floating] | None = None
-    spectrum_deg: npt.NDArray[np.floating] | None = None
-    spectrogram_db: npt.NDArray[np.floating] | None = None
-    spectrogram_frequencies: npt.NDArray[np.floating] | None = None
-    spectrogram_times: npt.NDArray[np.floating] | None = None
-    range_profiles_db: list[npt.NDArray[np.floating]] | None = None
-    azimuth_profiles_db: list[npt.NDArray[np.floating]] | None = None
-    range_profiles_deg: list[npt.NDArray[np.floating]] | None = None
-    range_profiles_norm_deg: list[npt.NDArray[np.floating]] | None = None
-    azimuth_profiles_deg: list[npt.NDArray[np.floating]] | None = None
-    azimuth_profiles_norm_deg: list[npt.NDArray[np.floating]] | None = None
-    target_phase_value_deg: float | None = None
-    target_doppler_centroid_Hz: float | None = None
-    range_polynomial_fit: Polynomial | None = None
-    azimuth_polynomial_fit: Polynomial | None = None
-    rng_spectrum_boundaries: tuple[float, float] | None = None
-    az_spectrum_boundaries: tuple[float, float] | None = None
-
-
-@dataclass
-class SpectralAnalysisBlockInfo:
-    """Block-wise Spectral Analysis info"""
-
-    block_num: int | None = None
-    first_az_line_block: int | None = None
-    lines_block: int | None = None
-    samples_block: int | None = None
-    doppler_centroid_mid_block: float | None = None
-    azimuth_frequency_axis: npt.NDArray[np.floating] | None = None
-    range_frequency_axis: npt.NDArray[np.floating] | None = None
-    spectrum_db: npt.NDArray[np.floating] | None = None
-    spectrum_deg: npt.NDArray[np.floating] | None = None
-    spectrogram_db: npt.NDArray[np.floating] | None = None
-    spectrogram_frequencies: npt.NDArray[np.floating] | None = None
-    spectrogram_times: npt.NDArray[np.floating] | None = None
-    range_profiles_db: list[npt.NDArray[np.floating]] | None = None
-    azimuth_profiles_db: list[npt.NDArray[np.floating]] | None = None
-    range_profiles_deg: list[npt.NDArray[np.floating]] | None = None
-    azimuth_profiles_deg: list[npt.NDArray[np.floating]] | None = None
