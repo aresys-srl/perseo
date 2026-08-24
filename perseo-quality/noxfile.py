90e04f2af0d42d23bc42ef45e483b425e4226a98
Giorgio Parma <giorgio.parma@aresys.it>
2026-08-24 10:39:27 +0200

    quality, removing nox from the whole project

--- perseo-quality/noxfile.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
-# SPDX-License-Identifier: MIT
-
-"""Automating python testing, PERSEO Quality Package"""
-
-import sys
-
-sys.path.append("..")
-import nox_common  # noqa: F401
